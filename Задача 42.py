171e54ba20bd48deadd18f215c36c175525204ac
R.Ovsiannikov <56169169+roma1201@users.noreply.github.com>
2019-10-07 12:37:15 +0300

    Add files via upload

---
+++ Задача 42.py
@@ -0,0 +1,11 @@
+x = int(input('Введите число: '))
+y = int(input('Введите число: '))
+x != y
+if x < y:
+    a = (x+y)/2
+    b = (x*y)*2
+    print('Если x меньше y:', a, b)
+if x > y:
+    b = (x+y)/2
+    a = (x*y)*2
+    print('Если x больше y:', a, b)
