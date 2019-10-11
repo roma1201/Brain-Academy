ad316fbeb875fb541c8fec1df9918b6e9b7b490a
R.Ovsiannikov <56169169+roma1201@users.noreply.github.com>
2019-10-11 17:06:38 +0300

    Add files via upload

---
+++ task18.py
@@ -0,0 +1,9 @@
+import math
+def task18(a1,b1,c1,r):
+    a=2*r*math.sin(a1)
+    b=2*r*math.sin(b1)
+    c=2*r*math.sin(c1)
+    print('сторона А=:',a)
+    print('сторона B=:',b)
+    print('сторона C=:',c)
+
