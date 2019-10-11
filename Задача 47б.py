ae82c2b7cb9e349c2b1bfa7750440e4309b99cb2
R.Ovsiannikov <56169169+roma1201@users.noreply.github.com>
2019-10-11 16:18:44 +0300

    Update Задача 47б.py

--- Задача 47б.py
+++ Задача 47б.py
@@ -1,8 +1,7 @@
 x=float(input('Введите число x:'))
 y=float(input('Введите число y:'))
 z=float(input('Введите число z:'))
-if (x + y > z):
-    (x + z > y) and (y + z > x):
+if (x + y > z) and (x + z > y) and (y + z > x):
     print('Остроугольный')
 else:
     print('Не остроугольный')
